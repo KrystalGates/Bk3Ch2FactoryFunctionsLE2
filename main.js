ae0d990d05dacab923502a6e3fc30c70682112d1
krystal gates <krystal_sabrina@yahoo.com>
2019-06-17 15:29:03 -0500

    complete exercise

--- main.js
+++ main.js
@@ -3,3 +3,16 @@
 // Pet name
 // Pet breed
 // Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels
+
+const createPet = (name, breed) => ({
+    "petName" : name,
+    "petBreed": breed
+})
+
+let bowWowKennels = []
+
+bowWowKennels.push(createPet( "Steve", "germansheperd"))
+bowWowKennels.push(createPet("Yum", "blue heeler"))
+bowWowKennels.push(createPet( "John", "golden retriever"))
+console.log(bowWowKennels)
+
