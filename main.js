027fda9bca13d1509c52b44380f36a008c1f43de
krystal gates <krystal_sabrina@yahoo.com>
2019-06-17 15:10:26 -0500

    initial HTML and exercise intructions

---
+++ main.js
@@ -0,0 +1,5 @@
+// Lightning Exercise 2: Write a factory function that creates an object that represents a pet. The function should accept two arguments.
+
+// Pet name
+// Pet breed
+// Invoke the factory function 3 times and place each animal in an array stored in a variable named BowWowKennels
